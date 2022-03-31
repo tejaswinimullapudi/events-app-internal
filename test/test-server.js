29d2e6c946d9bfe988c42a42d63f9916cd375d27
tejaswinimullapudi <tsrisai4@gmail.com>
2022-03-31 05:29:08 +0000

    some change

--- test/test-server.js
+++ test/test-server.js
@@ -57,7 +57,7 @@ describe('GET /', function() {
     it('adds an event', function(done) {
       request(app)
       .post('/event')
-      .send( { title: 'a test event', description: 'a really cool test' })
+      .send( { title: 'a test event', description: 'a really cool tes' })
       .set('Accept', 'application/json')
       .expect(200)
       .end((err, res) => {
